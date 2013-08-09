d01a9bf19fc985484598f5327ba3580405547312
Victor Vargas <v.vargas2009@gmail.com>
2013-08-09 13:10:28 -0500

    Changes to URL

--- lib/omniauth/strategies/heh.rb
+++ lib/omniauth/strategies/heh.rb
@@ -7,7 +7,7 @@ module OmniAuth
       option :name, "heh"
 
       option :client_options, {
-        :site          => 'http://calm-peak-1397.herokuapp.com',
+        :site          => 'http://localhost:3000',
         :request_token_path => '/oauth/request_token',
         :access_token_path  => '/oauth/access_token',
         :authorize_path     => '/oauth/authorize'
@@ -17,7 +17,6 @@ module OmniAuth
 
       credentials do
         hash = {'token' => access_token.token}
-        hash.merge!('refresh_token' => access_token.refresh_token) if access_token.refresh_token
         hash
       end
       
