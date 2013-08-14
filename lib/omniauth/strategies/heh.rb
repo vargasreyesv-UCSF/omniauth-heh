260d06223089571271e372601abeb27bdbad6d61
Victor Vargas <v.vargas2009@gmail.com>
2013-08-14 16:29:23 -0700

    Fixes to omniauth startegy

--- lib/omniauth/strategies/heh.rb
+++ lib/omniauth/strategies/heh.rb
@@ -11,22 +11,7 @@ module OmniAuth
         :request_token_path => '/oauth/request_token',
         :access_token_path  => '/oauth/access_token',
         :authorize_path     => '/oauth/authorize'
-      }
-
-      protected
-      
-      # customize this to return data on the user who has just logged in.
-      def user_data
-        @data ||= MultiJson.decode(@access_token.get('/users/1.json'))['user']
-      end
-
-      def auth_hash
-        OmniAuth::Utils.deep_merge(super, {
-          'uid' => user_data['id'],
-          'extra' => {'user_hash' => user_data}
-        })
-      end
-  
+      }  
     end
   end
 end
