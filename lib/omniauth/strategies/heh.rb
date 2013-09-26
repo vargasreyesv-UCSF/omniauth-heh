6eb3cbc2d7604d0a90fa834e5f984dad975d50af
Victor Vargas <v.vargas2009@gmail.com>
2013-09-26 14:44:34 -0500

    changes to the site URL

--- lib/omniauth/strategies/heh.rb
+++ lib/omniauth/strategies/heh.rb
@@ -7,7 +7,7 @@ module OmniAuth
       option :name, "heh"
  
       option :client_options, {
-        :site          => 'https://dev.health-eheartstudy.org',
+        :site          => 'http://localhost:3000',
         :request_token_path => '/oauth/request_token',
         :access_token_path  => '/oauth/access_token',
         :authorize_path     => '/oauth/authorize'
